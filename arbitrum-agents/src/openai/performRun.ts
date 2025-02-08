2ee1aefd33aab85bba081b9b6362dfc213d74334
dumprahul <shanrahul04@gmail.com>
2025-02-08 11:14:10 +0530

    agents

--- arbitrum-agents/src/openai/performRun.ts
+++ arbitrum-agents/src/openai/performRun.ts
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
 import { Thread } from "openai/resources/beta/threads/threads";
 import { Run } from "openai/resources/beta/threads/runs/runs";
-import { handleRunToolCalls } from "./handleRunToolCalls.js";
+import { handleRunToolCalls } from "./handleRunToolCall.js";
 
 export async function performRun(run: Run, client: OpenAI, thread: Thread) {
 

6101ec9a67111fe88694260b4257816ac333bf1e
dumprahul <shanrahul04@gmail.com>
2025-02-09 10:23:28 +0530

    gaiacall

--- arbitrum-agents/src/openai/createAssistant.ts
+++ arbitrum-agents/src/openai/createAssistant.ts
@@ -7,21 +7,14 @@ const apiKey = process.env.LLAMA_API_KEY || "your-api-key-here";
 
 export const client = new OpenAI({
     apiKey: apiKey,
-    baseURL: "https://llama8b.gaia.domains/v1" 
+    baseURL: "https://llama70b.gaia.domains/v1" 
 });
-
 
 export async function createAssistant(client: OpenAI): Promise<Assistant> {
     return await client.beta.assistants.create({
         model: "llama",
-        name: "Ajith",
+        name: "Cooper",
         instructions: assistantPrompt,
         tools: Object.values(tools).map(tool => tool.definition)
     });
 }
-
-
-
-
-
-//gaia-NmQyMDZjYjktNTJhNC00NjM5LTliMmUtMTgyZjI3NzM5ODZj-nnBOS6Fzf3Y7yp5f
